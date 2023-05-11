9ae9b09b9986637dc6a5a005bfd3764fe9c26698
Rahul Rajesh <rahulrajeshrr365@gmail.com>
2023-05-11 15:03:33 +0000

    structure creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { WorksComponent } from './works/works.component';
+import { ContactComponent } from './contact/contact.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    WorksComponent,
+    ContactComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
